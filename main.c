d1fd710ef871689bb667ed527419b3afadc209f9
gomsoup <gomsoup@gmail.com>
2019-12-25 05:58:32 +0900

    modifyElement() added

--- main.c
+++ main.c
@@ -9,9 +9,11 @@ list l;
 int main() {
 	makeTable(&l, GLOBAL, "global");
 	
-	int i = 0;
-	addElement(&l, INT, VARIABLE, "i", 0, nullptr, &i);
-	addElement(&l, INT, VARIABLE, "a", 0, nullptr, &i);
-	addElement(&l, INT, VARIABLE, "i", 0, nullptr, &i);
+	int i = 0, c[10] = {0,}, d[10] = {1,};
+	addElement(&l, INT, VARIABLE, "i", 0, nullptr, 0, &i);
+	addElement(&l, INT, VARIABLE, "a", 0, nullptr, 0, &i);
+	addElement(&l, INT, VARIABLE, "i", 0, nullptr, 0, &i);
+    addElement(&l, INT_ARRAY, VARIABLE, "c", 0, nullptr, sizeof(c) / sizeof(int), &c);
+    modifyElement(&l, INT_ARRAY, VARIABLE, "c", &d);
 
 }
--- symtable.c
+++ symtable.c
@@ -16,31 +16,36 @@ SYM_TABLE* createTable(TABLE _type, char* _name){
     return table;
 }
 
-SYM_SYMBOL* createSymbol( TYPE _type,  SYMBOL _sym_type, char* _name, int _param_cnt, TYPE* _param_types, void* _address){
+SYM_SYMBOL* createSymbol( TYPE _type,  SYMBOL _sym_type, char* _name, int _param_cnt, TYPE* _param_types, int _size, void* _address){
     SYM_SYMBOL* symbol = (SYM_SYMBOL *)malloc(sizeof(SYM_SYMBOL));
     
     symbol->type = _type;
-    symbol->name = _name;
+    symbol->name = strdup(_name);
     symbol->sym_type = _sym_type;
-    symbol->address= _address;
+    symbol->size = _size;
+
+    if(_sym_type == VARIABLE)
+        symbol->address = (int *)_address;
+    else
+        symbol->address = _address;
+
 
     if(_sym_type == T_FUNCTION){
         symbol->param_cnt = _param_cnt;
         symbol->param_types = ( TYPE *)malloc(_param_cnt * sizeof( TYPE *));
         for(int i=0; i < _param_cnt; i++)
             symbol->param_types[i] = _param_types[i];
-        
     }
 
     return symbol;
 }
 
-void* lookupFunction(list* list, TYPE _type, char* _name){
+SYM_SYMBOL* lookupFunction(list* list, TYPE _type, char* _name){
     SYM_TABLE* node = list->table;
 
     for(int i = 0; i < node->child_cnt; i++){
         if (!strcmp(_name, node->child[i]->name) && node->child[i]->sym_type == T_FUNCTION && node->child[i]->type == _type)
-            return node->child[i]->address;
+            return node->child[i];
     }
 
     if(list->prev != nullptr)
@@ -55,13 +60,13 @@ int addList(list* current, list* prev){
     return true;
 }
 
-void* lookupVariable(list* list, SYMBOL _type, char* _name)
+SYM_SYMBOL* lookupVariable(list* list, TYPE _type, char* _name)
 {
     SYM_TABLE* node = list->table;
 
     for (int i = 0; i < node->child_cnt; i++) {
         if (!strcmp(_name, node->child[i]->name) && node->child[i]->sym_type == VARIABLE && node->child[i]->type == _type)
-            return node->child[i]->address;
+            return node->child[i];
     }
 
     if (list->prev != nullptr)
@@ -78,17 +83,21 @@ void makeTable(list* list, TABLE _type, char* _name){
     list->table = createTable(_type, _name);
 }
 
-int addElement(list *list, TYPE _type, SYMBOL _sym_type, char* _name, int _param_cnt, TYPE* _param_types, void* _address) {
+int addElement(list *list, TYPE _type, SYMBOL _sym_type, char* _name, int _param_cnt, TYPE* _param_types, int _size, void* _address) {
     SYM_TABLE* table = list->table;
     
     if (_sym_type == VARIABLE) {
-        if (lookupVariable(list, _sym_type, _name) != nullptr) return 1;
+        if (lookupVariable(list, _type, _name) != nullptr) return 1;
 
         if(table->child_cnt == 0)
             table->child = (SYM_SYMBOL *)malloc(sizeof(SYM_SYMBOL));
         else
             realloc(table->child, sizeof(SYM_SYMBOL *) * (table->child_cnt+1));
-        table->child[table->child_cnt] = createSymbol(_type, _sym_type, _name, 0, nullptr, _address);
+
+        if(_type == INT_ARRAY)
+            table->child[table->child_cnt] = createSymbol(_type, _sym_type, _name, 0,  nullptr, _size, _address);
+        else
+            table->child[table->child_cnt] = createSymbol(_type, _sym_type, _name, 0, nullptr, 0, _address);
         table->child_cnt++;
     }
     else if (_sym_type == T_FUNCTION) {
@@ -98,9 +107,30 @@ int addElement(list *list, TYPE _type, SYMBOL _sym_type, char* _name, int _param
             table->child = (SYM_SYMBOL *)malloc(sizeof(SYM_SYMBOL));
         else
             realloc(table->child, (table->child_cnt+1) * sizeof(SYM_SYMBOL));
-        table->child[table->child_cnt] = createSymbol(_type, _sym_type, _name, _param_cnt, _param_types, _address);
+        table->child[table->child_cnt] = createSymbol(_type, _sym_type, _name, _param_cnt, _param_types, 0, _address);
         table->child_cnt++;
     }
 
     return true;
+}
+
+int modifyElement(list* list, TYPE _type, SYMBOL _sym_type, char* _name, void *value){
+    SYM_SYMBOL* sym;
+
+    if(_sym_type != VARIABLE) return false;
+
+    sym = lookupVariable(list, _type, _name);
+    if(sym == nullptr) return false;
+    if(_type != sym->type)
+
+    if(_type == INT)
+        sym->address = value;
+    if(_type == INT_ARRAY){
+        int *lv = (int *)sym->address;
+        int *rv = (int *)value;
+
+        for(int i=0; i < sym->size; i++)
+            lv[i] = rv[i];
+
+    }
 }
